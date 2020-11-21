ca00d6ceea9909db0583d654be83d57b87157dac
9541ad <wogns970@naver.com>
2020-11-21 21:06:14 +0900

    메인 xml에 카드뷰로 구성하고 그 안에 텍스트뷰로 버스번호를 출력하려고 했는데... 돌아가긴 합니다만 나타나질 않습니다..

--- bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/MainActivity.java
+++ bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/MainActivity.java
@@ -1,85 +1,62 @@
 package com.thinking.juicer.busstopapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.thinking.juicer.busstopapplication.R;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
+    String url = "http://openapitraffic.daejeon.go.kr/api/rest/busRouteInfo/getRouteInfoAll?reqPage=1&serviceKey=cC0rVYquPDL%2Bu44mxQ0ds5EabhA44uysOYBPVwBa0%2FeoGxSfKQgQCP4eCys0OB6VU6LUc9Ty2e%2BaBw7w61QB4g%3D%3D";
+    TextView bsNum;
 
-
-
-
-  TextView textView;
-     EditText et_search;
-     Button btn_search;
-    RecyclerView recycler_routesAll;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
-
-
+    private static String getTagValue(String tag, Element eElement){
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if(nValue == null) return null;
+        return nValue.getNodeValue();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        URL url;
-
-
+        bsNum = (TextView) findViewById(R.id.bs_num);
         try {
-            url = new URL("http://openapitraffic.daejeon.go.kr/api/rest/busRouteInfo/getRouteInfoAll?reqPage=1&serviceKey=cC0rVYquPDL%2Bu44mxQ0ds5EabhA44uysOYBPVwBa0%2FeoGxSfKQgQCP4eCys0OB6VU6LUc9Ty2e%2BaBw7w61QB4g%3D%3D");
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new InputSource(url.openStream()));
-            doc.getDocumentElement().normalize();
+            while (true) {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(url);
 
-            NodeList nodeList = doc.getElementsByTagName("item");
-
-            String s = "";
-
-            for(int i = 0; i< nodeList.getLength(); i++) {
-
-                Node node = nodeList.item(i);
-                Element fstElmnt = (Element) node;
-
-                NodeList ROUTE_NO = fstElmnt.getElementsByTagName("ROUTE_NO");
-                s += "ROUTE_NO = " + ROUTE_NO.item(0).getChildNodes().item(0).getNodeValue() + "\n";
+                NodeList nList = doc.getElementsByTagName("ROUTE_NO");
 
 
+                for (int i = 0; i < nList.getLength(); i++) {
+                    Node nNode = nList.item(i);
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eElement = (Element) nNode;
+
+                        bsNum.setText(getTagValue("ROUTE_NO",eElement));
+
+                    }
+                }
             }
-            textView.setText(s);
 
 
-        } catch (Exception e) {
-            Toast.makeText(getBaseContext(), "Parsing Error", Toast.LENGTH_SHORT).show();
         }
-
-
-
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
-
-
 }
+
+
