a7e5a9a0ed0a33de6049941e4b96824025a54be5
9541ad <wogns970@naver.com>
2020-11-25 20:35:29 +0900

    리스트뷰 버스번호 하나 가져오기 성공..

--- bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/MainActivity.java
+++ bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.thinking.juicer.busstopapplication.R;
@@ -16,118 +18,74 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+   ListView lv;
 
-    ArrayList<BusRouteItem> items= new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler);
+        lv = (ListView)findViewById(R.id.lv);
 
-        //대량의 데이터 추가 작업
-        readRss();
-    }//onCreate Method ..
+        ArrayList<BusRouteItem> list = xmlParser();
+        String[] data = new String[list.size()];
+        for(int i=0; i<list.size(); i++){
+            data[i] = list.get(i).getBusNum();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,data);
+        lv.setAdapter(adapter);
 
-    //rss xml문서 읽어와서 파싱하는 작업 메소드
-    void readRss(){
+    }
+
+    private ArrayList<BusRouteItem> xmlParser(){
+        ArrayList<BusRouteItem> arrayList = new ArrayList<BusRouteItem>();
+        InputStream is = getResources().openRawResource(R.raw.bus_route_info);
 
         try {
-            URL url=new URL("http://openapitraffic.daejeon.go.kr/api/rest/busRouteInfo/getRouteInfoAll?reqPage=1&serviceKey=cC0rVYquPDL%2Bu44mxQ0ds5EabhA44uysOYBPVwBa0%2FeoGxSfKQgQCP4eCys0OB6VU6LUc9Ty2e%2BaBw7w61QB4g%3D%3D");
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new InputStreamReader(is, "UTF-8"));
+            int eventType = parser.getEventType();
+            BusRouteItem busRouteItem = null;
 
-            //스트림역할하여 데이터 읽어오기 : 인터넷 작업은 반드시 퍼미션 작성해야함.
-            //Network작업은 반드시 별도의 Thread만 할 수 있다.
-            //별도의 Thread 객체 생성
-            RssFeedTask task= new RssFeedTask();
-            task.execute(url); //doInBackground()메소드가 발동[thread의 start()와 같은 역할]
-        } catch (MalformedURLException e) { e.printStackTrace();}
-
-    }// readRss Method ..
-
-    //이너 클래스
-    class RssFeedTask extends AsyncTask<URL, Void, String>{
-
-        //Thread의 run()메소드와 같은 역할
-        @Override
-        protected String doInBackground(URL... urls) { //...는 여러개를 받는 의미, 만약 task.execute(url, url2, url3); 보내면 urls[3]로 받는다.
-            //전달받은 URL 객체
-            URL url= urls[0];
-
-            //해임달(URL)에게 무지개로드(Stream) 열도록..
-            try {
-                InputStream is= url.openStream();
-
-                //읽어온 xml를 파싱(분석)해주는 객체 생성
-                XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
-                XmlPullParser xpp= factory.newPullParser();
-
-                //utf-8은 한글도 읽어오기 위한 인코딩 방식
-                xpp.setInput(is, "utf-8");
-                int eventType= xpp.getEventType();
-
-                BusRouteItem item= null;
-                String tagName= null;
-
-                while (eventType != XmlPullParser.END_DOCUMENT){
-                    switch (eventType){
-                        case XmlPullParser.START_DOCUMENT:
-                            break;
-                        case XmlPullParser.START_TAG:
-                            tagName=xpp.getName();
-
-                            if(tagName.equals("itemList")){
-                                item= new BusRouteItem();
-                            }else if(tagName.equals("ROUTE_NO")){
-                                xpp.next();
-                                if(item!=null) item.setBusNum(xpp.getText());
-                            }
-                            break;
-                        case XmlPullParser.TEXT:
-                            break;
-                        case XmlPullParser.END_TAG:
-                            tagName=xpp.getName();
-                            if(tagName.equals("itemList")){
-
-                                items.add(item);
-                                item=null;
-
-                                //리사이클러의 아답터에게 데이터가
-                                //변경되었다는 것을 통지
-
-                            }
-                            break;
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                switch (eventType){
+                    case XmlPullParser.START_TAG:
+                    String startTag = parser.getName();
+                    if(startTag.equals("msgBody")){
+                        busRouteItem = new BusRouteItem();
                     }
-                    eventType= xpp.next();
-                }//while
+                    if(startTag.equals("ROUTE_NO")){
+                        busRouteItem.setBusNum(parser.nextText());
+                    }
+                    break;
+                    case XmlPullParser.END_TAG:
+                        String endTag = parser.getName();
+                        if(endTag.equals("msgBody")){
+                            arrayList.add(busRouteItem);
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
 
-                //파싱 작업이 완료되었다!!
-                //테스트로 Toastㄹ 보여주기, 단 별도 스레드는
-                //UI작업이 불가! 그래서 runOnUiThread()를 이용했었음.
-                //이 UI작업을 하는 별도의 메소드로
-                //결과를 리턴하는 방식을 사용
-
-            } catch (IOException e) {e.printStackTrace();} catch (XmlPullParserException e) {e.printStackTrace();}
-
-            return "파싱종료"; // 리턴 값은 onPostExecute(String s) s에 전달된다.
-        }//doIBackground()
-
-        //doInBackground메소드가 종료된 후
-        //UI작업을 위해 자동 실행되는 메소드
-        //runOnUiThread()와 비슷한 역할
-        @Override
-        protected void onPostExecute(String s) { //매개 변수 s에 들어오는 값음 doIBackground()의 return 값이다.
-            super.onPostExecute(s);
-
-            //이 메소드 안에서는 UI변경 작업 가능
-            Toast.makeText(MainActivity.this, s+":"+items.size(), Toast.LENGTH_SHORT).show();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }//RssFeedTask class
+        return arrayList;
+    }
 
-}//MainActivity class ..
+}
