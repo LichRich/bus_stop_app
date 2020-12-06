874a9a98e4be1503996043b2fb8f0a45f487a13d
Subin Jeong <60subin@naver.com>
2020-12-06 22:27:56 +0900

    음성파일 추가

--- bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/CheckNotificationActivity.java
+++ bus_stop_application/app/src/main/java/com/thinking/juicer/busstopapplication/CheckNotificationActivity.java
@@ -3,6 +3,7 @@ package com.thinking.juicer.busstopapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -35,6 +36,8 @@ public class CheckNotificationActivity extends AppCompatActivity {
         no.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(), GetOffVoiceActivity.class);
+                startActivity(myintent);
                 finish();
             }
         });
